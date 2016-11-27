eb3904b0ab32fbbfba413622d2287b86d616a735
Junle Li <lijunle@gmail.com>
2016-11-27 23:06:27 +0800

    Bootstrap the logic.
    - Add self config file.
    - Leverage js-yaml to parse the config file.
    - Declare binary entry file.

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-module.exports = 'dependent-build';
+const fs = require('fs');
+const path = require('path');
+const yaml = require('js-yaml');
+
+function dependentBuild(folderPath) {
+  const cwd = process.cwd();
+  const configPath = path.resolve(cwd, folderPath, 'dependent-build.yml');
+  const configContent = fs.readFileSync(configPath, 'utf-8');
+  const config = yaml.safeLoad(configContent);
+  console.log(config);
+}
+
+module.exports = dependentBuild;
