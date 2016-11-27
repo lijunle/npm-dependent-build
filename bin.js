f29005cdd9e19df2b4f7edfb4455c6169df39e91
Junle Li <lijunle@gmail.com>
2016-11-28 02:29:13 +0800

    Add crazy logging information.
    - By default, only the info and upper level logs are shown.

--- bin.js
+++ bin.js
@@ -1,3 +1,14 @@
 #!/usr/bin/env node
 
-require('./index')(process.argv[2] || '');
+const pino = require('pino');
+const dependentBuild = require('./index');
+
+const pretty = pino.pretty();
+pretty.pipe(process.stdout);
+
+const logLevel = process.env.DEPENDENT_BUILD_LOG || 'info';
+const logger = pino({ name: 'dependent-build', level: logLevel }, pretty);
+const workingFolder = process.argv[2] || '';
+dependentBuild(logger, workingFolder)
+  .then(() => logger.info('Dependent build successfully'))
+  .catch(e => logger.error(e, 'Dependent build failed'));
--- index.js
+++ index.js
@@ -3,34 +3,62 @@ const path = require('path');
 const yaml = require('js-yaml');
 const childProcess = require('child_process');
 
-function createFolder(folder) {
-  return new Promise((resolve, reject) =>
-    fs.mkdir(folder, error => (error ? reject(error) : resolve())));
-}
-
-function cloneRepo(cwd, repo) {
+function createFolder(logger, folder) {
   return new Promise((resolve, reject) => {
-    const clone = childProcess.spawn('git', ['clone', repo], { cwd });
-    clone.on('error', reject);
-    clone.on('exit', code => (code ? reject(`Clone ${repo} exit code with ${code}`) : resolve()));
+    logger.debug({ folder }, 'Create folder');
+    fs.mkdir(folder, error => (error ? reject(error) : resolve()));
   });
 }
 
-function cloneRepos(workingFolder, repos) {
-  return cloneRepo(workingFolder, repos[0]); // TODO multiple repos
+function cloneRepo(logger, cwd, repo) {
+  return new Promise((resolve, reject) => {
+    logger.info({ cwd, repo }, 'Git clone repo start');
+
+    const clone = childProcess.spawn('git', ['clone', repo], { cwd });
+    clone.on('error', reject);
+    clone.on('exit', (code) => {
+      if (code) {
+        logger.error({ code }, 'Git clone failed');
+        reject(`Clone ${repo} exit code with ${code}`);
+      } else {
+        logger.info('Git clone successfully');
+        resolve();
+      }
+    });
+  });
 }
 
-function dependentBuild(folderPath) {
-  const cwd = process.cwd();
-  const rootFolder = path.resolve(cwd, folderPath);
-  const workingFolder = path.resolve(rootFolder, 'dependent-build');
-  const configPath = path.resolve(rootFolder, 'dependent-build.yml');
-  const configContent = fs.readFileSync(configPath, 'utf-8');
-  const config = yaml.safeLoad(configContent);
-  const repos = Object.keys(config);
+function cloneRepos(logger, workingFolder, repos) {
+  return cloneRepo(logger.child({ _cloneRepos: ['cloneRepo', 0] }), workingFolder, repos[0]); // TODO multiple repos
+}
 
-  return createFolder(workingFolder)
-    .then(() => cloneRepos(workingFolder, repos));
+function dependentBuild(logger, folderPath) {
+  const cwd = process.cwd();
+  logger.info({ cwd }, 'Current working directory');
+
+  const rootFolder = path.resolve(cwd, folderPath);
+  logger.debug({ rootFolder }, 'Resolved root directory');
+
+  const workingFolder = path.resolve(rootFolder, 'dependent-build');
+  logger.debug({ workingFolder }, 'Dependent-build folder');
+
+  const configPath = path.resolve(rootFolder, 'dependent-build.yml');
+  logger.info({ configPath }, 'Dependent-build configuration path');
+
+  const configContent = fs.readFileSync(configPath, 'utf-8');
+  logger.trace({ configContent }, 'Dependent-build configuration file content');
+
+  const config = yaml.safeLoad(configContent);
+  logger.debug({ config }, 'Dependent-build configuration');
+
+  const repos = Object.keys(config);
+  logger.trace({ repos }, 'Got dependent repos');
+
+  const batches = repos.map(repo => ({ repo, scripts: config[repo] }));
+  logger.trace({ batches }, 'Arrange config to batches (repo-scripts pairs)');
+
+  return createFolder(logger.child({ _dependentBuild: 'createFolder' }), workingFolder)
+    .then(() => cloneRepos(logger.child({ _dependentBuild: 'cloneRepos' }), workingFolder, repos));
 }
 
 module.exports = dependentBuild;
